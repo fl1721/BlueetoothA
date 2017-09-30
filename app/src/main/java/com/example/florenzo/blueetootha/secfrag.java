5cf4e2c02893ae4352eaad8a291ca9b572eb7c5a
f lorenzo <fl1721@jagmail.southalabama.edu>
2017-09-30 14:28:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/florenzo/blueetootha/secfrag.java
@@ -0,0 +1,30 @@
+package com.example.florenzo.blueetootha;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by florenzo on 9/29/2017.
+ */
+
+public class secfrag extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.layout2, container, false);
+        return myView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 2");
+    }
+}
